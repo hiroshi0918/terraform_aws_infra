a8c33c21514e15978a78687d111e6b1a00348752
寺嶋裕 <h_terashima@terashimahiroshis-Mac-mini.local>
2025-07-12 00:15:00 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.41.0"
+        }
+    }
+    cloud {
+      organization = "tera_terraform_practice"
+
+      workspaces {
+        name = "aws-infra"
+      }
+    }
+}
+
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+resource "aws_instance" "test_server" {
+    ami = "ami-0eba6c58b7918d3a1"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "TestInstance"
+    }
+}
+
